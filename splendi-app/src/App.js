ee408105ba3a44af81179114faf9cd5d8a17d878
Madeleine Depagnier <madeleine.depagnier@gmail.com>
2022-05-07 14:08:40 -0400

    added navbar, deleted extra files

--- splendi-app/src/App.js
+++ splendi-app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavbarComp from './components/NavbarComp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          testing yay cool wow
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <NavbarComp />
+      <Routes>
+        <Route path="/" exact />
+      </Routes>
+    </Router>
+
+    </>
   );
 }
 
