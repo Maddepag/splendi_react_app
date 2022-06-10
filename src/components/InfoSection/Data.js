313e220a5ac9b38cc59a30945cd48941f1197a13
Madeleine Depagnier <madeleine.depagnier@gmail.com>
2022-06-10 17:30:34 -0400

    delete node stuff

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,51 +1,51 @@
     
 
-export const homeObjOne = {
-    id: 'about',
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
-    topLine: 'about',
-    headline: 'splendi',
-    description: "splendi is an indie artist based out of philadelphia. she's working on writing music and finding ways to get it to new ears 24/7. thanks for stopping by.",
-    imgStart: false,
-    img: require('../../images/splendi_port_2.svg').default,
-    alt: 'Portrait',
-    dark: true,
-    primary: true,
-    darkText: false
-};
+// export const homeObjOne = {
+//     id: 'about',
+//     lightBg: false,
+//     lightText: true,
+//     lightTextDesc: true,
+//     topLine: 'about',
+//     headline: 'splendi',
+//     description: "splendi is an indie artist based out of philadelphia. she's working on writing music and finding ways to get it to new ears 24/7. thanks for stopping by.",
+//     imgStart: false,
+//     img: require('../../images/splendi_port_2.svg').default,
+//     alt: 'Portrait',
+//     dark: true,
+//     primary: true,
+//     darkText: false
+// };
 
-export const homeObjTwo = {
-    id: 'tour',
-    lightBg: true,
-    lightText: false,
-    lightTextDesc: false,
-    headline: 'upcoming shows',
-    description: "MAY 31 - j solomon w/ splendi and Strange Weekend at PhilaMOCA!",
-    buttonLabel: 'get tickets',
-    imgStart: false,
-    img: require('../../images/edit_philamoca.svg').default,
-    alt: 'tour_photo',
-    dark: false,
-    primary: false,
-    darkText: true,
-    buttonRoute: "https://dice.fm/partner/dice/event/d9d6o-j-solomon-31st-may-philamoca-philadelphia-tickets?dice_source=web&dice_medium=organic&dice_campaign=DICE&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1Q83NkwyMjFJSjUySAEAe%2FDKHiEAAAA%3D&_branch_match_i="
-};
+// export const homeObjTwo = {
+//     id: 'tour',
+//     lightBg: true,
+//     lightText: false,
+//     lightTextDesc: false,
+//     headline: 'upcoming shows',
+//     description: "MAY 31 - j solomon w/ splendi and Strange Weekend at PhilaMOCA!",
+//     buttonLabel: 'get tickets',
+//     imgStart: false,
+//     img: require('../../images/edit_philamoca.svg').default,
+//     alt: 'tour_photo',
+//     dark: false,
+//     primary: false,
+//     darkText: true,
+//     buttonRoute: "https://dice.fm/partner/dice/event/d9d6o-j-solomon-31st-may-philamoca-philadelphia-tickets?dice_source=web&dice_medium=organic&dice_campaign=DICE&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1Q83NkwyMjFJSjUySAEAe%2FDKHiEAAAA%3D&_branch_match_i="
+// };
 
-export const homeObjThree = {
-    id: 'merch',
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
-    headline: 'merch',
-    description: "shirts, totes, and more :)",
-    imgStart: false,
-    img: require('../../images/splendi_port_2.svg').default,
-    alt: 'Portrait',
-    dark: true,
-    primary: true,
-    darkText: false,
-    buttonLabel: 'view merch',
-    buttonRoute: 'https://www.instagram.com/splendiiii/?hl=en'
-};
+// export const homeObjThree = {
+//     id: 'merch',
+//     lightBg: false,
+//     lightText: true,
+//     lightTextDesc: true,
+//     headline: 'merch',
+//     description: "shirts, totes, and more :)",
+//     imgStart: false,
+//     img: require('../../images/splendi_port_2.svg').default,
+//     alt: 'Portrait',
+//     dark: true,
+//     primary: true,
+//     darkText: false,
+//     buttonLabel: 'view merch',
+//     buttonRoute: 'https://www.instagram.com/splendiiii/?hl=en'
+// };
